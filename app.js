4c4623bd70d295034d8e01b6d0751b140ebc9704
vignesh <vigneshbharathi68@hotmail.com>
2022-03-27 12:45:14 +0530

    Card design finished

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ let app = Vue.createApp({
         {name: "Home", icon: "/"},
         {name: "Home", icon: "/"},
         {name: "Home", icon: "/"},
+      ],
+      cardItems: [
+        { titleText:'ROI Income', numText:'4280.884 TRX', headIconPath: '/assets/img/roi_income.svg', mainIconPath: '/assets/img/roi_income-1.svg' }
       ]
     }
   }
